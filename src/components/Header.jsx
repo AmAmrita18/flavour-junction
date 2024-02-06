da2915c007eb7197cb233c9073175cf1a241260a
Amrita Gupta <amamrita0818@gmail.com>
2024-02-06 06:13:51 +0530

    update changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -79,7 +79,7 @@ const Header = () => {
             </div>
         </div>
         <div
-          className={`space-y-4 w-[85%]  rounded-2xl max-w-[1200px] mx-auto px-4 md:hidden mt-16 py-32 bg-[#393e41] opacity-90 ${
+          className={`space-y-4 w-[85%]  rounded-2xl max-w-[1200px] mx-auto px-4 md:hidden mt-12 py-28 bg-[#393e41] opacity-95 ${
             isMenuOpen ? "block fixed top-14 right-0 left-0" : "hidden"
           }`}
          
